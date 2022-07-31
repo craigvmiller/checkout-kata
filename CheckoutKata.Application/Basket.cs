c458c4e4731391c06f4eae2da4bfa2c3a9c9989b
Craig Cooper-Miller <craigvmiller@users.noreply.github.com>
2022-07-31 22:15:41 +0100

    Initial commit, created basic basket and item, unit test for adding item to basket

---
+++ CheckoutKata.Application/Basket.cs
@@ -0,0 +1,33 @@
+﻿namespace CheckoutKata.Application
+{
+    public class Basket
+    {
+        private ICollection<BasketItem> _items;
+        private int _total;
+
+        public ICollection<BasketItem> Items => _items;
+        public int Total => _total;
+
+        public Basket()
+        {
+            _items = new List<BasketItem>();
+        }
+
+        public void Add(BasketItem item)
+        {
+            _items.Add(item);
+            UpdateTotal();
+        }
+
+        private void UpdateTotal()
+        {
+            _total = _items.Sum(x => x.UnitPrice);
+        }
+    }
+
+    public class BasketItem
+    {
+        public string SKU { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
---
+++ CheckoutKata.Tests/BasketTests.cs
@@ -0,0 +1,29 @@
+﻿using CheckoutKata.Application;
+using NUnit.Framework;
+
+namespace CheckoutKata.Tests
+{
+    [TestFixture]
+    public class BasketTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void Can_Add_Item()
+        {
+            var item = new BasketItem
+            {
+                SKU = "A",
+                UnitPrice = 10,
+            };
+
+            var basket = new Basket();
+            basket.Add(item);
+
+            Assert.AreEqual(1, basket.Items.Count);
+        }
+    }
+}
