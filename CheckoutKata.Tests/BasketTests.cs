c407b6dfec0ec7b712fc6a265a3a97386001514e
Craig Cooper-Miller <craigvmiller@users.noreply.github.com>
2022-07-31 22:44:16 +0100

    Added test for basket total calculation, tests now run successfully

--- CheckoutKata.Tests/BasketTests.cs
+++ CheckoutKata.Tests/BasketTests.cs
@@ -6,12 +6,7 @@ namespace CheckoutKata.Tests
     [TestFixture]
     public class BasketTests
     {
-        [SetUp]
-        public void SetUp()
-        {
-        }
-
-        [Test]
+        [TestCase]
         public void Can_Add_Item()
         {
             var item = new BasketItem
@@ -24,6 +19,28 @@ namespace CheckoutKata.Tests
             basket.Add(item);
 
             Assert.AreEqual(1, basket.Items.Count);
+        }
+
+        [TestCase]
+        public void Calculates_Total()
+        {
+            var item1 = new BasketItem
+            {
+                SKU = "A",
+                UnitPrice = 10,
+            };
+
+            var item2 = new BasketItem
+            {
+                SKU = "B",
+                UnitPrice = 15,
+            };
+
+            var basket = new Basket();
+            basket.Add(item1);
+            basket.Add(item2);
+
+            Assert.AreEqual(25, basket.Total);
         }
     }
 }
